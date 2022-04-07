585365af8e09ec90f42d65c7031446e263642c4b
davincecode <vncntybnz@gmail.com>
2022-04-07 00:22:33 +0000

    Update exercise 6 hourly rate

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,12 +11,12 @@ puts "----------"
 # Your code goes here ...
 # create employee data
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "Lily", last_name: "Aldridge", hourly_rate: 36)
+@store1.employees.create(first_name: "Lily", last_name: "Aldridge", hourly_rate: 63)
 @store1.employees.create(first_name: "Alessandra", last_name: "Ambrosio", hourly_rate: 40)
 @store2.employees.create(first_name: "Tyra", last_name: "Banks", hourly_rate: 48)
-@store2.employees.create(first_name: "Leomie", last_name: "Anderson", hourly_rate: 29)
-@store2.employees.create(first_name: "Elsa", last_name: "Hosk", hourly_rate: 33)
-@store4.employees.create(first_name: "Taylor", last_name: "Hill", hourly_rate: 26)
-@store4.employees.create(first_name: "Martha", last_name: "Hunt", hourly_rate: 32)
+@store2.employees.create(first_name: "Leomie", last_name: "Anderson", hourly_rate: 92)
+@store2.employees.create(first_name: "Elsa", last_name: "Hosk", hourly_rate: 63)
+@store4.employees.create(first_name: "Taylor", last_name: "Hill", hourly_rate: 62)
+@store4.employees.create(first_name: "Martha", last_name: "Hunt", hourly_rate: 62)
 @store5.employees.create(first_name: "Adriana", last_name: "Lima", hourly_rate: 40)
 @store6.employees.create(first_name: "Heidi", last_name: "Klum", hourly_rate: 48)
